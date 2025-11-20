f6c5eece9e3b7882103b08a4ee4b163ccc533802
taimourz <taimour.rkt@gmail.com>
2025-11-20 20:00:59 +0500

    driver files

---
+++ main_with_file_io.cpp
@@ -0,0 +1,19 @@
+/*  *
+    *
+    * Use this file when we want to do file io
+    * use fin instead  of cin
+    * use fout instead of cout
+    *
+    *
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main()
+{
+
+}
---
+++ main_without_file_io.cpp
@@ -0,0 +1,13 @@
+/*  *
+    *
+    * Use this file when we dont want to do any file io
+    *
+    *
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+
+}
